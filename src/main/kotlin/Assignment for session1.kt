f038a63c8693ad62105cb81067b2a0f2f5a10404
JJOMBWE-INNOVATIVE <140011996+JJOMBWE-INNOVATIVE@users.noreply.github.com>
2023-07-22 23:04:17 +0300

    kotlin assignmens

---
+++ src/main/kotlin/Assignment for session1.kt
@@ -0,0 +1,26 @@
+import kotlin.math.sqrt
+
+// ASSIGNMENT 1
+
+// Number 1
+    fun main() { println("Enter current year")
+        val g = readln().toInt()
+         println("Enter the year you were born")
+        val f = readln().toInt()
+        val h = g - f
+
+        print(h)
+
+    val a = 4
+    val b = 3
+    val d = a*a + b*b
+    val e = d.toFloat()
+    val c = sqrt(e)
+    print(c.toInt())
+}
+
+
+
+
+
+
