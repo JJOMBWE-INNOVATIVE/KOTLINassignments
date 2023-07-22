6c533c0077ff0cff1fd00f28f768635b8bab5d18
JJOMBWE NATHAN <140011996+JJOMBWE-INNOVATIVE@users.noreply.github.com>
2023-07-22 23:28:54 +0300

    Add files via upload

---
+++ KOTLINassignments/src/main/kotlin/Assignment for session1.kt
@@ -0,0 +1,27 @@
+import kotlin.math.sqrt
+
+// ASSIGNMENT 1
+
+// Number 1
+    fun main() { println("Enter current year")
+        val g = readln().toInt()
+         println("Enter the year you were born")
+        val f = readln().toInt()
+        val h = g - f
+
+        println(h)
+
+// number 2
+    val a = 4
+    val b = 3
+    val d = a*a + b*b
+    val e = d.toFloat()
+    val c = sqrt(e)
+    println(c.toInt())
+}
+
+
+
+
+
+
