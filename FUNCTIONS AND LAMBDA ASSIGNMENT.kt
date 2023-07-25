31a84c8c7e08ce8fd86ddd19757982e1e5482369
JJOMBWE NATHAN <140011996+JJOMBWE-INNOVATIVE@users.noreply.github.com>
2023-07-25 22:08:47 +0300

    Update FUNCTIONS AND LAMBDA ASSIGNMENT.kt

--- FUNCTIONS AND LAMBDA ASSIGNMENT.kt
+++ FUNCTIONS AND LAMBDA ASSIGNMENT.kt
@@ -1,3 +1,13 @@
+//QUESTION
+//write a function that has three parameters; two doubles and one character
+//(*, +, -, /) and checks if third parameter
+// == *, it will multiply the two doubles
+// == +, it will add the two numbers
+// == -, it will subtract the two numbers
+// == /, it will divide the two numbers    
+
+//ANSWER
+
 fun main(){
     println("Enter first num")
     val fistnumber = readln().toDouble()
